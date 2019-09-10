c3660239c3a25da265fd1688256e05c98d031450
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-09-10 15:56:01 +0300

    Create task3.cpp

---
+++ task3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float a, b, c, ac, bc, axc;
+
+	cout << "Введите координаты точек A,B и C" << endl;
+	cin >> a >> b >> c;
+
+	if (c > b && c < a)
+	{
+		cout << "Точка С не между A и B";
+	}
+
+	else
+	{
+		ac = fabs(c - a);
+		bc = fabs(c - b);
+		axc = ac * bc;
+
+		cout << "Произведение AC и BC равно " << axc << endl;
+	}
+
+	system("pause");
+	return 0;
+}
+
+//даны три точки а,б,с, на числовой оси. Точка с между А и Б. Найти произведение длин отрезков АС и БС
+//даны коорды двух противоположных вершин пря
