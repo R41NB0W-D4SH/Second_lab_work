cf921cf5ef01a9038f2006ac1624c17506651f2c
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-09-13 12:54:07 +0200

    Create task4.cpp

---
+++ task4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double x1,x2,x3,x4,y1,y2,y3,y4,p,s;
+	cout <<"Введите координаты противоположных вершин прямоугольника для первой точки (x1,y1) и (x2,y2)";
+	cin >> x1 >> y1 >> x2 >> y2;
+
+	if (x1 == x2 || y1 == y2) 
+
+	system("pause");
+	return 0;
+}
