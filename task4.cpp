0e6bf69099b959803ef6a94171d6302893fc99c8
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-09-16 13:29:18 +0300

    Update task4.cpp

--- task4.cpp
+++ task4.cpp
@@ -6,12 +6,25 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	double x1,x2,x3,x4,y1,y2,y3,y4,p,s;
-	cout <<"Введите координаты противоположных вершин прямоугольника для первой точки (x1,y1) и (x2,y2)";
+	double x1, x2, y1, y2, a, b, p, s;
+	cout << "Введите координаты противоположных вершин прямоугольника для первой точки (x1,y1) и (x2,y2)" << endl;
 	cin >> x1 >> y1 >> x2 >> y2;
 
-	if (x1 == x2 || y1 == y2) 
+	if (x1 == x2 || y1 == y2)
+		{
+			cout << "Стороны прямоугольника не параллельны осям координат" << endl;
+		}
 
-	system("pause");
+	else
+		{
+		a = sqrt(pow(y2 - y1, 2));
+		b = sqrt(pow(x2 - x1, 2));
+		p = (a + b) / 2;
+		s = a * b;
+
+		cout << "Периметр равен " << p << endl << "Площадь равна " << s << endl;
+		}
+
+		system("pause");
 	return 0;
 }
